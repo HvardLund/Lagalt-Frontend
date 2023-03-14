906b2444c98f96574c3f8e85303693f420f7ea98
Håvard Lund <havard.lund@no.experis.com>
2023-03-14 18:46:14 +0100

    feat: added member list component

---
+++ src/components/memberList/index.jsx
@@ -0,0 +1,19 @@
+import styles from './memberList.module.css'
+//import FeatherIcon from 'feather-icons-react';
+
+function MemberList(props) {
+
+    const memberList = props.members? props.members:[]
+
+    return(
+        <div className={styles.container}>
+            {memberList.map(member =>
+            <div className={styles.member}>
+                <img className={styles.profilePhoto} src={member.profileImage} alt='avatar' />
+                <div className={styles.username}>{member.username}</div>
+            </div>
+            )}
+        </div>
+    )
+}
+export default MemberList
--- src/pages/projectpage/index.jsx
+++ src/pages/projectpage/index.jsx
@@ -1,4 +1,5 @@
 import DescriptionTextField from '../../components/descriptionTextField';
+import MemberList from '../../components/memberList';
 import ProgressBar from '../../components/progressBar';
 import styles from './projectpage.module.css'
 
@@ -6,6 +7,24 @@ const intro='Bli med på et sci-fi-basert rollespill og utforsk ulike planeter o
 const description='Vi er en gruppe med lidenskapelige spillutviklere som arbeider med å utvikle et spennende nytt spill kalt "Galactic Quest". Dette er et sci-fi-basert rollespill der spillerne kan utforske ulike planeter og galakser, møte forskjellige karakterer og delta i episke kamper. Målet vårt er å lage et spill som både er underholdende og utfordrende, og som vil engasjere spillere over hele verden.\nSom en del av teamet vil du ha muligheten til å bidra til utviklingen av ulike aspekter av spillet, inkludert design av figurer og landskap, opprettelse av historiefortelling og utvikling av gameplay-mekanismer. Vi ser etter personer med forskjellige ferdigheter, fra programmering og kunstnerisk design til historiefortelling og lyddesign.\nProsjektet er hobbybasert, så det vil ikke være noen økonomisk kompensasjon for deltakelse. Men det vil være en fantastisk mulighet til å utvikle ferdighetene dine, samarbeide med lidenskapelige og talentfulle mennesker og bygge en portefølje av arbeid som kan hjelpe deg i din fremtidige karriere.\nHvis du er interessert i å bli med i teamet vårt, kan du kontakte oss for mer informasjon om hvordan du kan delta. Vi ser frem til å høre fra deg og å jobbe sammen for å skape en fantastisk spillopplevelse for spillere over hele verden!'
 const projectImage = 'Prosjektbilde.png'
 const status = 'In progress'
+
+const memberList = [
+    {
+        fullname: 'Petter Pettersen',
+        username: 'Petter sprett',
+        profileImage: 'profile.svg'
+    },
+    {
+        fullname: 'Hans Hansen',
+        username: 'Hansen',
+        profileImage: 'profile.svg'
+    },
+    {
+        fullname: 'Ola Nordmann',
+        username: 'Bola Ola',
+        profileImage: 'profile.svg'
+    },
+]
 
 function ProjectPage(){
 
@@ -24,7 +43,10 @@ function ProjectPage(){
                 <DescriptionTextField type='description' content={description}/>
             </div>
             <div className={`${styles.rightColumn} ${styles.column}`}>
-                <div className={styles.contentCard}><h2 className={styles.subHeader}>Members</h2></div>
+                <div className={styles.contentCard}>
+                    <h2 className={styles.subHeader}>Members</h2>
+                    <MemberList members={memberList}/>
+                </div>
                 <div className={styles.contentCard}><h2 className={styles.subHeader}>Skills</h2></div>
                 <div className={styles.contentCard}><h2 className={styles.subHeader}>Tags</h2></div>
             </div>
